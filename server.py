f264415c5b619a8071e1c758a0aa73ce5e19edc4
Emile Thigpen <emilethigpen@gmail.com>
2021-07-11 18:22:52 -0500

    works on odds now not evens though

--- server.py
+++ server.py
@@ -7,11 +7,30 @@ def checkerboard():
 
 @app.route('/<int:rowx>')
 def checkerboard2(rowx):
-    return render_template('index.html', row=rowx, column=8)
+    boxxes= rowx*8
+    sizex=8*75
+    if(boxxes%2==0):
+        boxxes=int(boxxes/2)
+        return render_template('index.html', boxes=boxxes, extrabox=0, size=sizex)
+    elif(boxxes%2!=0):
+        boxxes -=1
+        boxxes=int(boxxes/2)
+        extraboxx=1
+        return render_template('index.html', boxes=boxxes, extrabox=extraboxx, size=sizex)
+
 
 @app.route('/<int:row>/<int:column>')
 def checkerboard3(row,column):
-    return render_template('index.html', row=row, column=column)
+    boxes= row*column
+    size=column*75
+    if(boxes%2==0):
+        boxes=int(boxes/2)
+        return render_template('index.html', boxes=boxes, extrabox=0, size=size)
+    elif(boxes%2!=0):
+        boxes -=1
+        boxes=int(boxes/2)
+        extrabox=1
+        return render_template('index.html', boxes=boxes, extrabox=extrabox, size=size)
 
 
 
