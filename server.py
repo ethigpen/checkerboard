85e49231ea782001296c5fde56b6d92be23891c7
Emile Thigpen <emilethigpen@gmail.com>
2021-07-11 22:50:45 -0500

    all good to go!

--- server.py
+++ server.py
@@ -3,38 +3,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def checkerboard():
-    return render_template('index.html', row=8, column=8)
+    return render_template('index.html', row=8, column=8, color1='red', color2='black')
 
 @app.route('/<int:rowx>')
 def checkerboard2(rowx):
-    boxxes= rowx*8
-    sizex=8*75
-    if(boxxes%2==0):
-        boxxes=int(boxxes/2)
-        return render_template('index.html', boxes=boxxes, extrabox=0, size=sizex)
-    elif(boxxes%2!=0):
-        boxxes -=1
-        boxxes=int(boxxes/2)
-        extraboxx=1
-        return render_template('index.html', boxes=boxxes, extrabox=extraboxx, size=sizex)
+    return render_template('index.html', row=rowx, column=8, color1='red', color2='black')
 
 
 @app.route('/<int:row>/<int:column>')
 def checkerboard3(row,column):
-    boxes= row*column
-    size=column*75
-    if(boxes%2==0):
-        boxes=int(boxes/2)
-        return render_template('index.html', boxes=boxes, extrabox=0, size=size)
-    elif(boxes%2!=0):
-        boxes -=1
-        boxes=int(boxes/2)
-        extrabox=1
-        return render_template('index.html', boxes=boxes, extrabox=extrabox, size=size)
+    return render_template('index.html', row=row, column=column, color1='red', color2='black')
 
-
-
-
+@app.route('/<int:rowc>/<int:columnc>/<string:color1>/<string:color2>')
+def checkerboard4(rowc,columnc,color1,color2):
+    return render_template('index.html', row=rowc, column=columnc, color1=color1,color2=color2)
 
 if __name__=="__main__":
     app.run(debug=True)
